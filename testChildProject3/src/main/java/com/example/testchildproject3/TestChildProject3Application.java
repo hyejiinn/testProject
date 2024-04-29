7f1cf48af884da03141f576293fab8906e3daa44
hyejin1231 <phj981231@naver.com>
2024-04-29 11:45:16 +0900

    test Child Project3 commit

---
+++ testChildProject3/src/main/java/com/example/testchildproject3/TestChildProject3Application.java
@@ -0,0 +1,16 @@
+package com.example.testchildproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestChildProject3Application
+{
+	
+	public static void main(String[] args)
+	{
+		SpringApplication.run(TestChildProject3Application.class, args);
+		System.out.println("TestChildProject3Application.main");
+	}
+	
+}
---
+++ testChildProject3/src/test/java/com/example/testchildproject3/TestChildProject3ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.testchildproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestChildProject3ApplicationTests
+{
+	
+	@Test
+	void contextLoads()
+	{
+	}
+	
+}
